fd1e9a3ee250a2b0242ab43d0c4d9cdc64be8fae
Ignacio Carbajo <icarbajop@gmail.com>
2016-03-02 23:15:53 +0000

    Add examples

--- js/libraries/ramda/tests/sandbox.js
+++ js/libraries/ramda/tests/sandbox.js
@@ -1,15 +1,100 @@
 import { expect } from "chai"
 import R from "ramda"
 
+const checkEqlSpread = ({ fn, expected, initSpread }) => {
+  const actual = fn(...initSpread)
+
+  expect(actual).to.eql(expected)
+}
+
+const checkEql = ({ init, fn, expected }) => {
+  checkEqlSpread({ initSpread: [init], fn, expected })
+}
 
 describe("sandbox", () => {
   it("1", () => {
+    const initSpread = [3, 4, 9, -3]
+
     const { juxt, unapply } = R
 
-    const range = unapply(juxt)(Math.min, Math.max)
-    const actual = range(3, 4, 9, -3)
+    const fn = unapply(juxt)(Math.min, Math.max)
     const expected = [-3, 9]
 
-    expect(actual).to.eql(expected)
+    checkEqlSpread({ fn, expected, initSpread })
+  })
+
+  it("2", () => {
+    const { is, always, of, over, inc, lensIndex, ifElse } = R
+
+    const ifElseIsArray = ifElse(is(Array))
+
+    const incrementHead = ifElseIsArray(
+      over(lensIndex(0), inc),
+      always(of(1))
+    )
+
+    const actual1 = incrementHead({ foo: "bar" })
+    const expected1 = [1]
+    expect(actual1).to.eql(expected1)
+
+    const actual2 = incrementHead([1, 2])
+    const expected2 = [2, 2]
+    expect(actual2).to.eql(expected2)
+  })
+
+  it("3", () => {
+    const init = "foobar"
+    const expected = "FoObAr"
+
+    const {
+      invoker, addIndex, map, complement, modulo, compose,
+      ifElse, toUpper, identity, nthArg, __,
+    } = R
+
+    // split :: String → [String]
+    const split = invoker(1, "split")("")
+    // join :: [String] → String
+    const join = invoker(1, "join")("")
+    // mapIndexed :: Functor f => (a, int → b) → f a → f b
+    const mapIndexed = addIndex(map)
+    // isEven :: Number → Boolean
+    const isEven = complement(modulo(__, 2))
+    // secondArgumentIsEven :: a → b → Number
+    const secondArgumentIsEven = compose(isEven, nthArg(1))
+    // upperOdd :: [String] → [String]
+    const upperOdd = mapIndexed(ifElse(secondArgumentIsEven, toUpper, identity))
+    // fn :: String → String
+    const fn = compose(join, upperOdd, split)
+
+    checkEql({ init, fn, expected })
+  })
+
+  it("4", () => {
+    const {
+      always, ifElse, compose, equals, modulo, __, unapply,
+      apply, over, lensIndex,
+    } = R
+
+
+    // isInt :: Number → Boolean
+    const isInt = compose(equals(0), modulo(__, 1))
+    // isInt :: [(=> a), (=> b)] → a|b
+    const applyIfElseIsInt = apply(ifElse(isInt))
+    // isInt :: [a, b] → [a, b]
+    const callAlwaysOnSecondArg = over(lensIndex(1), always)
+    // recursivelyCallIfInt :: ((Number => Number), Number) → Number → Number
+    const recursivelyCallIfInt = R.curryN(2, unapply(compose(
+      applyIfElseIsInt,
+      callAlwaysOnSecondArg
+    )))
+
+    const fn = num => R.converge(
+      R.uncurryN(2, recursivelyCallIfInt(fn)),
+      [R.identity, Math.sqrt]
+    )(num)
+
+    // minimum int with recursive sqrt
+    checkEql({ init: 81, fn, expected: 3 })
+    checkEql({ init: 16, fn, expected: 2 })
   })
 })
