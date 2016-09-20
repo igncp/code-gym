5c93021ce4a1010d9f25052a4ba582e75880c59a
Ignacio Carbajo <icarbajop@gmail.com>
2016-09-20 21:41:16 +0200

    Start python project

---
+++ python/csv/src/look-for-year.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+"""
+This script accepts a CSV file with a shape like
+the sample. It will try to guess the release year
+of the album and append it as the third column,
+otherwise it will add "-". The second argument is
+the output file, which will be written entirely each
+time a year is found.
+"""
+
+import sys
+import csv
+import discogs_client
+import time
+
+source_file_path = sys.argv[1]
+target_file_path = sys.argv[2]
+
+lines = []
+with open(source_file_path, 'rb') as csvfile:
+    content = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for line in content:
+        lines.append(line)
+
+discogs_token = None
+with open("discogs-token.txt", "rb") as file:
+    discogs_token = file.read().rstrip()
+
+# generate a user token at https://www.discogs.com/settings/developers
+d = discogs_client.Client('LookForAlbumYearApplication/0.1',
+    user_token=discogs_token)
+
+
+def write_file():
+    with open(target_file_path, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+            quotechar='"', quoting=csv.QUOTE_ALL)
+        for album in lines:
+            writer.writerow(album)
+
+# requests are throttled to 240 per minute
+
+lines_length = len(lines)
+for idx, album in enumerate(lines):
+    artist = album[0]
+    title = album[1]
+    if len(album) > 2:
+        continue
+    results = d.search(artist + " " + title, type='master', per_page="100")
+    if len(results) > 0:
+        main_release = results[0].main_release
+        year = int(main_release.year)
+        time.sleep(2)
+    else:
+        year = "-"
+    lines[idx] = [artist, title, year]
+    print("Completed " + str(idx + 1) + " of " + str(lines_length))
+    print("Writing file ...")
+    write_file()
